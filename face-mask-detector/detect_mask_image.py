38c124b1c44434df3dacfaffc63fb40c0476277e
laygond <laygond_bryan@hotmail.com>
2020-05-12 05:14:20 -0400

    :pencil: update for different version of TF

--- face-mask-detector/detect_mask_image.py
+++ face-mask-detector/detect_mask_image.py
@@ -5,13 +5,13 @@
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
-#from tensorflow import ConfigProto
 import numpy as np
 import argparse
 import cv2
 import os
 
 # Prevent tensorflow from allocating the totality of a GPU memory
+# from tensorflow import ConfigProto
 # config = ConfigProto()
 # config.gpu_options.allow_growth = True
 
